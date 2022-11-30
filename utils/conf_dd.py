dec951c6f7d8e07e43b538d100c5cdea94645559
bs <bsantraigi@gmail.com>
2022-11-30 10:38:23 +0530

    conf file for each dataset

--- utils/conf_dd.py
+++ utils/conf_dd.py
@@ -30,20 +30,20 @@ import os
 
 class disc_config(object):
     batch_size = 256
-    lr = 0.2
+    lr = 0.01
     lr_decay = 0.9
     vocab_size = 25000
     embed_dim = 512
     steps_per_checkpoint = 200
     #hidden_neural_size = 128
     num_layers = 2
-    train_dir = './disc_data/'
-    name_model = "disc_model"
-    tensorboard_dir = "./tensorboard/disc_log/"
+    train_dir = './dd/disc_data/'
+    name_model = "disc_model_dd"
+    tensorboard_dir = "./tensorboard/dd_disc_log/"
     name_loss = "disc_loss"
     max_len = 50
     piece_size = batch_size * steps_per_checkpoint
-    piece_dir = "./disc_data/batch_piece/"
+    piece_dir = "./dd/disc_data/batch_piece/"
     #query_len = 0
     valid_num = 100
     init_scale = 0.1
@@ -56,17 +56,17 @@ class disc_config(object):
 
 
 class gen_config(object):
-    beam_size = 7
-    learning_rate = 0.5
+    beam_size = 4
+    learning_rate = 0.01
     learning_rate_decay_factor = 0.99
     max_gradient_norm = 5.0
     batch_size = 128
     emb_dim = 512
     num_layers = 2
     vocab_size = 25000
-    train_dir = "./gen_data/"
+    train_dir = "./dd/gen_data/"
     name_model = "st_model"
-    tensorboard_dir = "./tensorboard/gen_log/"
+    tensorboard_dir = "./tensorboard/dd_gen_log/"
     name_loss = "gen_loss"
     teacher_loss = "teacher_loss"
     reward_name = "reward"
@@ -97,17 +97,17 @@ class gen_config(object):
 
 
 class GSTConfig(object):
-    beam_size = 7
-    learning_rate = 0.5
+    beam_size = 4
+    learning_rate = 0.01
     learning_rate_decay_factor = 0.99
     max_gradient_norm = 5.0
     batch_size = 256
     emb_dim = 1024
     num_layers = 2
     vocab_size = 25000
-    train_dir = "./gst_data/"
-    name_model = "st_model"
-    tensorboard_dir = "./tensorboard/gst_log/"
+    train_dir = "./gst_data/dd/"
+    name_model = "st_model_dd"
+    tensorboard_dir = "./tensorboard/dd_gst_log/"
     name_loss = "gst_loss"
     max_train_data_size = 0
     steps_per_checkpoint = 200
