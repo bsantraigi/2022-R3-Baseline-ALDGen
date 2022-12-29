18b2e009930360188209a03b1da47a97f99b0dce
bs <bsantraigi@gmail.com>
2022-12-29 12:09:17 +0530

    changes: reformat to json

--- reformat_to_r3_json.py
+++ reformat_to_r3_json.py
@@ -12,10 +12,12 @@ from transformers import AutoTokenizer
 
 def cmdline_args():
     p = argparse.ArgumentParser()
-    p.add_argument('-src', '--source', required=True, help="""Source sequence to decode (one line per sequence)""")
+    p.add_argument('-src', '--source', required=True, help="""Source sequence (one line per sequence)""")
     # p.add_argument('-ctx', '--context', required=True, help="""Source sequence to decode (one line per sequence)""")
     p.add_argument('-tgt', '--target', required=True, help='True target sequence (optional)')
-    p.add_argument('-pred', '--prediction', required=True, help="""Path to output the predictions (each line will be the decoded sequence""")
+    p.add_argument('-pred', '--prediction', required=True, help="Path to output predictions file (each line will be one decoded sequence")
+    p.add_argument('-jp', '--json_path', required=True, help="Path for the output json file (will be created by this script)")
+    
     
     return p.parse_args()
 
@@ -24,12 +26,17 @@ print(f"Arguments: {args}")
 
 def read_array_file(fname):
     a = []
+    bad_s = 0
     with open(fname) as f:
         for line in f:
             line = line.strip()
             if line == "":
-                break
+                line = "the is" # some bad sentence
+                bad_s += 1
+            # if line == "":
+                # break
             a.append(line)
+    print(f"{bad_s} empty sentences in {fname}")
     return a
 
 # ctx = read_array_file(args.context)
@@ -37,12 +44,12 @@ src = read_array_file(args.source)
 tgt = read_array_file(args.target)
 pred = read_array_file(args.prediction)
 
-assert len(tgt) == len(src)
-assert len(pred) == len(tgt)
+assert len(tgt) == len(src), f"LT: {len(tgt)}, LS: {len(src)}, LP: {len(pred)}"
+assert len(pred) == len(tgt), f"LT: {len(tgt)}, LS: {len(src)}, LP: {len(pred)}"
 
-vocab_model_reference = 'facebook/blenderbot-3B'
-tokenizer = AutoTokenizer.from_pretrained(vocab_model_reference, use_fast=True, verbose=False)
-SEP = tokenizer.sep_token
+#vocab_model_reference = 'facebook/blenderbot-3B'
+#tokenizer = AutoTokenizer.from_pretrained(vocab_model_reference, use_fast=True, verbose=False)
+#SEP = tokenizer.sep_token
 
 collector_json = []
 for s,t,p in zip(src, tgt, pred):
@@ -52,5 +59,5 @@ for s,t,p in zip(src, tgt, pred):
         'adalabel': p
     })
     
-with open(args.prediction + '.json', 'w') as f:
+with open(args.json_path, 'w') as f:
     json.dump(collector_json, f)
